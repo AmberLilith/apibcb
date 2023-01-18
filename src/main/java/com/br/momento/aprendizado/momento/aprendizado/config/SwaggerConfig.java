3001f160ff9a7234dd02bc0c59f061bdeb0bccb9
Amber Lilith O. da Silva <amber.lilith.ordone@gmail.com>
2023-01-18 20:28:49 -0300

    alterações

--- src/main/java/com/br/momento/aprendizado/momento/aprendizado/config/SwaggerConfig.java
+++ src/main/java/com/br/momento/aprendizado/momento/aprendizado/config/SwaggerConfig.java
@@ -28,7 +28,7 @@ public class SwaggerConfig {
 
     private ApiInfo metaInfo() {
 
-        ApiInfo apiInfo = new ApiInfo(
+        return new ApiInfo(
                 "Produtos API REST",
                 "API REST de cadastro de produtos.",
                 "1.0",
@@ -38,8 +38,6 @@ public class SwaggerConfig {
                 "Apache License Version 2.0",
                 "https://www.apache.org/licesen.html", new ArrayList<VendorExtension>()
         );
-
-        return apiInfo;
     }
 
 }
